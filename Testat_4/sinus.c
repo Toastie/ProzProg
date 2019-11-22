f333ff7d92db38c8b410bc8f643d99659bcd0e83
Simon <32514734+Toastie@users.noreply.github.com>
2019-11-22 13:37:10 +0100

    sinus.c Optimiert

--- Testat_4/sinus.c
+++ Testat_4/sinus.c
@@ -2,25 +2,9 @@
 #include <stdint.h>
 #include <math.h>
 
-long double eexp(long double x, long double y){        //x hoch y
-    long double eexp = 1;
-    for (int i = 1; i <= y; i++){
-        eexp = eexp * x;
-    }
-    return(eexp);
-}
-
-long double fak(long double x){  //Fakultät von X
-    long double fak = 1;
-    for (int i = x; i > 1; i--){
-        fak = fak * i;
-    }
-    return(fak);
-}
-
 
 int main(){
-    double x,y,yold,k,summe,n;
+    double x,y,yold,k,summe;
     //Abfrage des X Wertes
     printf("Sin(x); x = ");
     scanf("%lf",&x);
@@ -34,10 +18,8 @@ int main(){
     k++;
     while (y != yold){
         yold = y;
-        //Berechnung des Nenners um nicht doppelt zu arbeiten
-        n = 2 * k + 1;
-        //Berechnung des nächsten Kettenglieds
-        summe = eexp(-1,k)*(eexp(x,n)/fak(n));
+        //Berechnung des nächsten Kettenglieds mittels -altes Kettenglied * neuer anteil 
+        summe = (-summe)*((x*x)/((2*k+1)*(2*k)));
         //Berechnung der Summe
         y = y + summe;
         //Ausgabe des Zwischenergebnis

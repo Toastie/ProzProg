cd6c89e747bfb0e0896452259d284d21c6ed30aa
Simon <32514734+Toastie@users.noreply.github.com>
2019-11-26 16:57:53 +0100

    Testat 5 abgeschlossen

--- Testat_5/sudoku.c
+++ Testat_5/sudoku.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int sudoku[9][9]={
     {4,1,0,0,6,5,0,0,7},
@@ -11,6 +12,7 @@ int sudoku[9][9]={
     {0,2,0,0,1,8,6,4,0},
     {6,0,0,3,0,0,0,1,0}
     };
+
     int original[9][9]={
     {4,1,0,0,6,5,0,0,7},
     {0,0,6,0,0,7,4,8,0},
@@ -53,11 +55,71 @@ void printSudoku(int sudoku[9][9]){
     }
 }
 
+int eingabePruefen(int x, int y, int z){
+    //Spalte überprüfen
+    for (int i = 0; i < 9; i++){
+        if (z == sudoku[i][y]){
+            return(1);
+        }
+    }
+
+    //Zeile überprüfen
+    for (int i = 0; i < 9; i++){
+        if (z == sudoku[x][i]){
+            return(2);
+        }
+    }
+
+    //Block finden und anfangskoordinate als x / y Wert setzen
+    printf("%d %d\n",x,y);
+    if(x <= 2){
+        x = 0;
+    }
+    else if(x <= 5){
+        x = 3;
+    }
+    else{
+        x = 6;
+    }
+
+    if(y <= 2){
+        y = 0;
+    }
+    else if(y <= 5){
+        y = 3;
+    }
+    else{
+        y = 6;
+    }
+    
+    printf("%d ",x);
+    printf("%d\n",y);
+    //Block überprüfen
+    for (int i = x; i < (x + 3); i++){
+        for (int j = y; j < (y + 3); j++){
+            if(z == sudoku[i][j]){
+                return(3);
+            }
+        }   
+    }
+    return(4);
+}
+
+int nullenVorhanden(int sudoku[9][9]){
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++){
+            if (sudoku[i][j] == 0){
+                return(1);
+            }
+        }
+    }
+    return(0);
+}
 int main(){
     int x,y,z;
     x = y = z = 0;
     printSudoku(sudoku);
-    while(1){
+    while(nullenVorhanden(sudoku)){
         printf("Zeile:");
         scanf("%d",&x);
         printf("Spalte:");
@@ -68,8 +130,12 @@ int main(){
         y--;
         if(x < 9 && y < 9 && x >= 0 && y >= 0 && z < 10 && z > 0){
             if(original[x][y] == 0){
-                sudoku[x][y] = z;
-                printSudoku(sudoku);
+                switch (eingabePruefen(x,y,z)){
+                case 1: printf("Eingabe ungültig, Zahl in der Spalte bereits vorhanden.\n"); break;
+                case 2: printf("Eingabe ungültig, Zahl in der Zeile bereits vorhanden.\n"); break;
+                case 3: printf("Eingabe ungültig, Zahl im Block bereits vorhanden.\n"); break;
+                case 4: system("@cls||clear"); sudoku[x][y] = z; printSudoku(sudoku); break;
+                }
             }
             else{
                 printf("Dieser Wert darf nicht verändert werden!\n");
@@ -79,5 +145,6 @@ int main(){
             printf("Ihre Eingaben liegen nicht im Wertebereich [1,9]\n");
         }
     }
+    printf("Congratulations, you've won!");
     return(0);
-}
+}  
